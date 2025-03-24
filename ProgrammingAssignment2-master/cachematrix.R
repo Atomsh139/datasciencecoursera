b8679258641a91cef7e16b2b1a3f7245777520b6
Atomsh <atomsh139@gmail.com>
2025-03-24 10:54:26 +0100

    Commit for Coursera week 3

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # Invalide le cache si on modifie la matrice
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)  
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)  # Calcule l'inverse
+  x$setinverse(inv)       # Stocke l'inverse en cache
+  inv
+}
